279d864ad1607a6422fd1f2a30f38800841aabd3
teresa100 <t_vikstedt@hotmail.com>
2017-09-26 18:20:26 +0200

    loop, rabatter etc

--- program.py
+++ program.py
@@ -67,6 +67,68 @@ book = Product(price=100, amount=1, tax=1.06)
 print(robot.price_with_tax() + book.price_with_tax())
 
 
+class Product:
+	def __init__(self, price, amount, tax):
+		self.price = price
+		self.amount = amount
+		self.tax = tax
+
+	def price_with_tax(self):
+		return self.price * self.amount * self.tax 
+
+
+products = [Product(price=900, amount=2, tax=1.25), Product(price=100, amount=1, tax=1.06)]
+total_price = products[0].price_with_tax() + products[1].price_with_tax()
+print(total_price)
+
+products = [Product(price=900, amount=2, tax=1.25), Product(price=100, amount=1, tax=1.06)]
+total_price = 0
+for product in products: 
+	total_price = total_price + product.price_with_tax()
+print(total_price)
+
+
+class Product:
+	def __init__(self, price, amount, tax):
+		self.price = price
+		self.amount = amount
+		self.tax = tax
+
+	def price_with_tax(self):
+		return self.price * self.amount * self.tax 
+
+products = [Product(price=900, amount=2, tax=1.25), Product(price=100, amount=1, tax=1.06)]
+total_price = 0
+for product in products: 
+	total_price = total_price + product.price_with_tax()
+
+if total_price > 500:
+	print(total_price * 0.9)
+
+class Product:
+	def __init__(self, name, price, amount, tax):
+		self.name = name
+		self.price = price
+		self.amount = amount
+		self.tax = tax
+
+	def price_with_tax(self):
+		total = self.price * self.amount * self.tax 
+		if total > 500:
+			return 0.9 * total
+		else:
+			return total 
+
+products = [Product(name="Robot", price=900, amount=2, tax=1.25), Product(name="Book", price=100, amount=1, tax=1.06), Product(name="batteries", price=300, amount=1, tax=1.25)]
+total_price = 0
+for product in products: 
+	total_price = total_price + product.price_with_tax()
+print(total_price)
+
+for product in products:
+	print(product.name, product.price_with_tax())
+
+
 
 
 
